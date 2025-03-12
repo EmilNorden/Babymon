29c56ed5973e2da97fe6f52aa5fabbede9e5e1f5
Emil Nordén <mail@emilnorden.se>
2025-03-12 19:32:46 +0100

    added react frontend

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
+mod types;
+
 use axum::{
     routing::{get, post},
     http::StatusCode,
     Json, Router,
 };
+use axum::http::Response;
+use axum::response::IntoResponse;
 use serde::{Deserialize, Serialize};
-
+use tower_http::cors::{Any, CorsLayer};
+use crate::types::Person;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
 
+    let cors = CorsLayer::new().allow_origin(Any);
+
     let app = Router::new()
-        .route("/", get(root));
+        .route("/", get(root))
+        .route("/people", get(get_people))
+        .layer(cors);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
@@ -22,4 +31,26 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn get_people() -> (StatusCode, Json<Vec<Person>>) {
+    let people = vec![
+        Person {
+            name: String::from("Person A"),
+            age: 36,
+            favourite_food: Some(String::from("Pizza")),
+        },
+        Person {
+            name: String::from("Person B"),
+            age: 5,
+            favourite_food: Some(String::from("Broccoli")),
+        },
+        Person {
+            name: String::from("Person C"),
+            age: 100,
+            favourite_food: None,
+        },
+    ];
+
+    (StatusCode::OK, Json(people))
 }
